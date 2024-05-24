7ee5517af28e2bbf9f849d355b8ea8680c5bcf8f
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-24 10:03:08 -0600

    strings: substrings, prefujos y sifijos

--- playground.playground/Contents.swift
+++ playground.playground/Contents.swift
@@ -1,6 +1,5 @@
 //: [Previous] (@previous)
+
 var greeting = "Hello, playground"
-
-
 
 //: [Next] (@next)
--- playground.playground/Sources/strings/strings.swift
+++ playground.playground/Sources/strings/strings.swift
@@ -65,4 +65,59 @@ func mainStr() {
     
     // inserting and removing
     welcome.insert("!", at: welcome.endIndex)
+    
+    
+    // substring
+    let idx = greeting.firstIndex(of: ",") ?? greeting.endIndex
+    let beginning = greeting[..<idx]
+
+    let newString = String(beginning)
+    print(newString)
+
+
+    // comparing string
+    let someText = "We're a lot alike, you and I."
+    let sameQuotation = "We're a lot alike, you and I."
+
+    if someText == sameQuotation {
+        print("These two strings are considered equal")
+    }
+
+    // prefijos y sufijos
+    let romeoAndJuliet = [
+        "Act 1 Scene 1: Verona, A public place",
+        "Act 1 Scene 2: Capulet's mansion",
+        "Act 1 Scene 3: A room in Capulet's mansion",
+        "Act 1 Scene 4: A street outside Capulet's mansion",
+        "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+        "Act 2 Scene 1: Outside Capulet's mansion",
+        "Act 2 Scene 2: Capulet's orchard",
+        "Act 2 Scene 3: Outside Friar Lawrence's cell",
+        "Act 2 Scene 4: A street in Verona",
+        "Act 2 Scene 5: Capulet's mansion",
+        "Act 2 Scene 6: Friar Lawrence's cell"
+    ]
+
+    var mansionCount = 0
+    var cellCount = 0
+
+    for scene in romeoAndJuliet {
+        if scene.hasSuffix("Capulet's mansion") {
+            mansionCount += 1
+        }
+        else if scene.hasSuffix("cell") {
+            cellCount += 1
+        }
+    }
+
+    print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+
+    var act1SceneCount = 0
+    for scene in romeoAndJuliet {
+        if scene.hasPrefix("Act 1") {
+            act1SceneCount += 1
+        }
+    }
+    print("There are \(act1SceneCount) scenes in Act 1")
+
 }
